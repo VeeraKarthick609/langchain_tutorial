7b7beef5227882aff828d778972ecfbe5e199016
Veera <veerakarthick609@gmail.com>
2024-12-23 06:45:43 +0530

    fixed typo in langchain tracking

--- chatbot/local_ollama.py
+++ chatbot/local_ollama.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv; load_dotenv(os.path.join(os.path.dirname(__file_
 
 
 # langsmith tracking
-os.environ["LANGCHAIN_TRACKING_V2"] = "true"
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
 # prompt template
